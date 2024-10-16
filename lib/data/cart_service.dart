50512a30fff1fde28227232a6efa376e20c149e1
REZWANULLAH QURAISHI <rezwankhanq@gmail.com>
2024-10-17 00:18:07 +0300

    models and data done

--- lib/data/cart_service.dart
+++ lib/data/cart_service.dart
@@ -1,5 +1,25 @@
 import 'package:bloc/models/cart.dart';
 
 class CartService {
-  static List<Cart> cartitems = List.empty();
+  static List<Cart> cartitems = List.empty(growable: true);
+
+  static final CartService _singlton = CartService._internal();
+
+  factory CartService() {
+    return _singlton;
+  }
+
+  CartService._internal();
+
+  static void addCart(Cart item) {
+    cartitems.add(item);
+  }
+
+  static void removeCart(Cart item) {
+    cartitems.remove(item);
+  }
+
+  static List<Cart> getCart() {
+    return cartitems;
+  }
 }
